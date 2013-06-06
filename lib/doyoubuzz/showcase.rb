15594755abe8957c5865828b96a96272cfc77743
David RUYER <david.ruyer@gmail.com>
2013-06-06 12:25:18 +0200

    First commit

---
+++ lib/doyoubuzz/showcase.rb
@@ -0,0 +1,7 @@
+require "doyoubuzz/showcase/version"
+
+module Doyoubuzz
+  module Showcase
+    # Your code goes here...
+  end
+end
---
+++ lib/doyoubuzz/showcase/version.rb
@@ -0,0 +1,5 @@
+module Doyoubuzz
+  module Showcase
+    VERSION = "0.0.1"
+  end
+end
