4689644b09877195a858125641316584c08c3da9
Thomas Larrieu <thomas.larrieu@gmail.com>
2017-02-07 17:39:20 +0100

    Fix code style

--- lib/doyoubuzz/showcase.rb
+++ lib/doyoubuzz/showcase.rb
@@ -6,11 +6,10 @@ require 'doyoubuzz/showcase/error'
 
 module Doyoubuzz
   class Showcase
-
-    Hashie.logger = ::Logger.new(STDOUT)
-
     include HTTParty
     base_uri 'http://showcase.doyoubuzz.com/api/v1'
+
+    Hashie.logger = ::Logger.new(STDOUT)
 
     # Construction with mandatory api key and api secret
     def initialize(api_key, api_secret)
@@ -19,40 +18,51 @@ module Doyoubuzz
     end
 
     # HTTP calls => forwarded to #call_api with the verb as the first argument
-    [:get, :post, :put, :delete].each do |verb|
-      define_method(verb) do |method, params = {}|
+    %i(get post put delete).each do |verb|
+      define_method(verb) do |method, params={}|
         call_api(verb, method, params)
       end
     end
 
     # SSO redirection
-    def sso_redirect_url(application_name, timestamp, sso_key, user_attributes)
-      # Check mandatory attributes are given
-      missing_attributes = [:email, :external_id, :firstname, :lastname].reject{|key| user_attributes[key]}
+    def sso_redirect_url(application, timestamp, sso_key, user_attributes)
+      enforce_sso_attributes(user_attributes)
 
-      if missing_attributes.length > 0
-        raise ArgumentError, "Missing mandatory attributes for SSO : #{missing_attributes.join(', ')}"
-      end
+      params = sign_sso_params(
+        user_attributes.merge(timestamp: timestamp),
+        sso_key
+      )
+      encoded_params = URI.encode_www_form(params)
 
-      params = sign_sso_params(user_attributes.merge(timestamp: timestamp), sso_key)
-
-      "http://showcase.doyoubuzz.com/p/fr/#{application_name}/sso?#{URI.encode_www_form(params)}"
+      "http://showcase.doyoubuzz.com/p/fr/#{application}/sso?#{encoded_params}"
     end
-
 
     private
 
+    def enforce_sso_attributes(attributes)
+      required = %i(email external_id firstname lastname)
+      missing = required.reject { |key| attributes[key] }
+
+      raise(
+        ArgumentError,
+        "Missing mandatory attributes for SSO : #{missing.join(', ')}"
+      ) if missing.any?
+    end
+
     # The actual api call
     def call_api(verb, method, params)
-      res = self.class.send(verb, method, build_request_parameters(params, verb))
-      return process_response(res)
+      res = self.class.send(
+        verb,
+        method,
+        build_request_parameters(params, verb)
+      )
+
+      process_response(res)
     end
 
     # Process the HTTParty response, checking for errors
     def process_response(res)
-      if !res.success?
-        raise Error.new(res.code, res.body)
-      end
+      raise Error.new(res.code, res.body) unless res.success?
 
       object = res.parsed_response
 
@@ -69,22 +79,20 @@ module Doyoubuzz
 
     # Build the request parameters
     def build_request_parameters(params, verb)
-      additional_parameters = {:apikey => @api_key, :timestamp => Time.now.to_i}
+      additional_parameters = { apikey: @api_key, timestamp: Time.now.to_i }
 
       # GET, DELETE requests : the parameters are in the request query
-      if [:get, :delete].include? verb
-        return {:query => sign_api_params(params.merge additional_parameters)}
+      if %i(get delete).include?(verb)
+        return { query: sign_api_params(params.merge(additional_parameters)) }
+      end
 
       # Otherwise, they are in the body
-      else
-        return {:body => params, :query => sign_api_params(additional_parameters)}
-      end
+      { body: params, query: sign_api_params(additional_parameters) }
     end
-
 
     # The arguments processing and signing
     def sign_api_params(params)
-      ordered_params_values = params.sort.map{|k,v|v}
+      ordered_params_values = params.sort.map { |_, v| v }
       concatenated_params_string = ordered_params_values.join
       concatenated_params_string << @api_secret
 
@@ -95,14 +103,17 @@ module Doyoubuzz
     # Different ordering
     def sign_sso_params(params, sso_key)
       # Custom ordering
-      tosign = params.values_at(:email, :firstname, :lastname, :external_id, :"group[]", :user_type, :timestamp).compact.join
-      tosign += sso_key
+      tosign = params.values_at(
+        :email,
+        :firstname,
+        :lastname,
+        :external_id,
+        :"group[]",
+        :user_type,
+        :timestamp
+      ).compact.join + sso_key
 
-      hash = Digest::MD5.hexdigest(tosign)
-
-      params.merge(hash: hash)
+      params.merge(hash: Digest::MD5.hexdigest(tosign))
     end
-
-
   end
 end
